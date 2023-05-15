8bd1709fc7aeeb33737542e00ab05860973b6ea3
cesarorozco9628 <paulocesarorozcoguerrero@hotmail.com>
2023-05-15 07:57:16 -0600

    prod

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ const put_params_bulding = (data) => {
 
     if (/Mobile/i.test(navigator.userAgent)) {
         let params_mob = params.length > 0 ? `?${params}` : ''
-      let lnk = `https://uat.morgana.mx/lola/registra_cotiza/${params_mob}`;
+      let lnk = `https://morgana.mx/lola/registra_cotiza/${params_mob}`;
       window.open(lnk, '_blank');
     } else {
       width_container = 'div-width'
@@ -49,7 +49,7 @@ const put_params_bulding = (data) => {
       <div class="d-flex justify-content-end pb-2 ${width_container}" id="id_container_close">
           <img src="./img/ICONO_CLOSE.svg" class="cursor-pointer" onclick="close_modal_mgn('id_fiv_frm_mgn')">
       </div>
-      <iframe class="${width_iframe}" src="https://uat.morgana.mx/lola/registra_cotiza/${params.length>0 ? `?${params}`:''}" id="id_ifm_mgn" width=""></iframe>
+      <iframe class="${width_iframe}" src="https://morgana.mx/lola/registra_cotiza/${params.length>0 ? `?${params}`:''}" id="id_ifm_mgn" width=""></iframe>
       `
       open_modal_mgn(id);
     }
